d8dbbb75e5913528d96d5f11d9f4c3e73e1ee79f
FilipeeOliveira <filipe.jx.10@gmail.com>
2024-07-15 09:40:01 -0400

    criacao de maquinas1

--- assets/js/page/features-post-create.js
+++ assets/js/page/features-post-create.js
@@ -39,19 +39,25 @@ document.getElementById('machineForm').addEventListener('submit', function(event
 
   // Realiza o envio dos dados utilizando fetch
   fetch('/machine/create', {
-    method: 'POST',
-    body: new FormData(this)
+      method: 'POST',
+      body: new FormData(this)
   })
   .then(response => {
-    if (response.ok) {
-      alert('Dados enviados com sucesso!');
-      // Aqui você pode redirecionar o usuário para outra página, se desejar
-    } else {
-      throw new Error('Erro ao enviar os dados.');
-    }
+      if (response.ok) {
+          alert('Dados enviados com sucesso!');
+          // Limpa os campos do formulário após o envio bem-sucedido
+          this.reset(); // Resetará os campos do formulário
+
+          // Se estiver utilizando Summernote para o campo de descrição, resete-o explicitamente
+          $('.summernote-simple').summernote('reset'); // Reseta o conteúdo do Summernote
+
+          // Você pode adicionar aqui qualquer outra ação após o envio bem-sucedido
+      } else {
+          throw new Error('Erro ao enviar os dados.');
+      }
   })
   .catch(error => {
-    console.error('Erro:', error);
-    alert('Ocorreu um erro ao enviar os dados. Por favor, tente novamente.');
+      console.error('Erro:', error);
+      alert('Ocorreu um erro ao enviar os dados. Por favor, tente novamente.');
   });
 });
