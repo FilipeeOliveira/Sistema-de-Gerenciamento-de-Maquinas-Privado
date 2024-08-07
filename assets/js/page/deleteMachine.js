2f5bbe1be0fc1106983ba4caad9ebe05387ec9be
FilipeeOliveira <filipe.jx.10@gmail.com>
2024-08-07 09:19:47 -0400

    modal de confirmcao de delete

--- assets/js/page/deleteMachine.js
+++ assets/js/page/deleteMachine.js
@@ -1,33 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let machineIdToDelete = null;
+  
+    // Abre o modal de confirmação quando o botão de deletar é clicado
     document.querySelectorAll('.delete-machine').forEach(button => {
-        button.addEventListener('click', function (event) {
-            event.preventDefault();
-            const machineId = this.getAttribute('data-id');
-            if (confirm('Tem certeza que deseja deletar esta máquina?')) {
-                fetch(`/machines/delete/${machineId}`, {
-                    method: 'DELETE',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-                    .then(response => {
-                        if (!response.ok) {
-                            return response.json().then(err => { throw new Error(err.message); });
-                        }
-                        return response.json();
-                    })
-                    .then(data => {
-                        if (data.success) {
-                            const row = document.getElementById(`machine-row-${machineId}`);
-                            if (row) {
-                                row.remove();
-                            }
-                        } else {
-                            alert('Erro ao deletar a máquina.');
-                        }
-                    })
-                    .catch(error => console.error('Error:', error));
-            }
-        });
+      button.addEventListener('click', function(event) {
+        event.preventDefault();
+        machineIdToDelete = this.getAttribute('data-id');
+        $('#deleteConfirmationModal').modal('show');
+      });
     });
-});
+  
+    // Lida com a confirmação de exclusão no modal
+    document.getElementById('confirmDeleteButton').addEventListener('click', async function() {
+      if (machineIdToDelete) {
+        try {
+          const response = await fetch(`/machines/delete/${machineIdToDelete}`, {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          });
+  
+          if (response.ok) {
+            const row = document.getElementById(`machine-row-${machineIdToDelete}`);
+            if (row) {
+              row.remove();
+            }
+            $('#deleteConfirmationModal').modal('hide');
+          } else {
+            const data = await response.json();
+            alert('Erro ao deletar a máquina: ' + data.message);
+          }
+        } catch (error) {
+          console.error('Error:', error);
+          alert('Erro ao deletar a máquina. Tente novamente.');
+        }
+      }
+    });
+  });
+  
