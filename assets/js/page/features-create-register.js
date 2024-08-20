c8c21f28fc6b64a367f5f4bb8d32daba3c562232
FilipeeOliveira <filipe.jx.10@gmail.com>
2024-08-20 11:11:59 -0400

    modal de manutencao - imagens sendo adicionadas sem serem substituidas, campo sendo resetado / filtro - filtro de documentos no log / contorno azul no filtro da lista de maquinas corrigido

--- assets/js/page/features-create-register.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('image-upload').addEventListener('change', function(event) {
-    const [file] = this.files;
-    if (file) {
-      document.getElementById('profile-preview').src = URL.createObjectURL(file);
-    }
-  });
